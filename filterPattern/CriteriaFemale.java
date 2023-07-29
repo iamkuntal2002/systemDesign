6813fe014bd4f665cd8206a5ac5730455a068a55
kuntal <iamkuntal2002@gmail.com>
2023-07-29 23:35:21 +0530

    29/07/2023

---
+++ filterPattern/CriteriaFemale.java
@@ -0,0 +1,18 @@
+package filterPattern;
+
+import java.util.List;
+import java.util.ArrayList;
+public class CriteriaFemale implements Criteria {
+
+   @Override
+   public List<Person> meetCriteria(List<Person> persons) {
+      List<Person> femalePersons = new ArrayList<Person>(); 
+      
+      for (Person person : persons) {
+         if(person.getGender().equalsIgnoreCase("FEMALE")){
+            femalePersons.add(person);
+         }
+      }
+      return femalePersons;
+   }
+}
